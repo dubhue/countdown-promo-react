9a6dc5f260f02cf8597b8b13a592a79eb50ff42b
peterbdub <peterbdub@gmail.com>
2019-09-28 21:33:47 -0600

    Plural case for “days”

--- PromoBar/PromoBarParts/Expires.js
+++ PromoBar/PromoBarParts/Expires.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
 const Expires = props => {
-    const padNum = num => {
-      return num < 10 ? "0" + num : num;
-    };
-    const timeString = exp => {
-      const days = Math.floor(exp / (1000 * 60 * 60 * 24)),
-        hours = Math.floor((exp % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-        minutes = Math.floor((exp % (1000 * 60 * 60)) / (1000 * 60)),
-        seconds = Math.floor((exp % (1000 * 60)) / 1000);
-      const daysStr = days > 0 ? " in " + days + " days " : "";
-      const hoursStr = hours > 0 ? padNum(hours) + ":" : "";
-      const minsStr = minutes > 0 ? padNum(minutes) + ":" : "";
-      return daysStr + hoursStr + minsStr + padNum(seconds);
-    };
-    return props.expires ? (
-      <div className="expires">| Expires {timeString(props.expires)}</div>
-    ) : null;
+  const padNum = num => {
+    return num < 10 ? "0" + num : num;
   };
+  const timeString = exp => {
+    const days = Math.floor(exp / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((exp % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes = Math.floor((exp % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds = Math.floor((exp % (1000 * 60)) / 1000);
+    const plural = days > 1 ? "days" : "day"
+    const daysStr = days ? " in " + days + " "+plural+" " : "";
+    const hoursStr = hours ? padNum(hours) + ":" : "";
+    const minsStr = minutes ? padNum(minutes) + ":" : "";
+    return daysStr + hoursStr + minsStr + padNum(seconds);
+  };
+  return props.expires ? (
+    <div className="expires">| Expires {timeString(props.expires)}</div>
+  ) : null;
+};
 
 export default Expires
